194dc4b91e92ca4e285540faa2e1b870f3468fc4
Wellington Lucena <wellingtondol.mw@gmail.com>
2024-03-02 11:57:46 -0300

    Instanciando as Classes

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploFundamentos.Common.Models;
+
+Calculadora calc = new Calculadora();
+Pessoa      pess = new Pessoa();
+
